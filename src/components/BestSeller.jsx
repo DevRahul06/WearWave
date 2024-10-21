cc43362dfc042651d438011960c4ce801d08e599
Rahul <rahulchavda6000@gmail.com>
2024-10-21 15:49:25 +0530

    Remove Consol.log

--- src/components/BestSeller.jsx
+++ src/components/BestSeller.jsx
@@ -13,7 +13,6 @@ const BestSeller = () => {
     setBestSeller(bestProduct);
   }, []);
 
-  console.log(bestSeller)
   return (
     <div className="my-10">
       <div className="text-center text-3xl py-8">
